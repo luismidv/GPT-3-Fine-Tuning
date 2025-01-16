0f969b6a1bfae39e92a1c1a4afb2f174b4604461
Luis Miguel Pérez <perezmartluismiguel@gmail.com>
2025-01-16 22:44:44 +0100

    Add files via upload

--- dataprepare.py
+++ dataprepare.py
@@ -5,30 +5,33 @@ from transformers import GPT2Model
 from datasets import load_dataset
 import evaluate
 
-dataset = load_dataset("bitext/Bitext-customer-support-llm-chatbot-training-dataset")
 
 
-def dataset_info(dataset):
-    data = pd.read_csv(dataset)
-    print(data.columns)
-    print(data.describe())
+
+
 
 def function_tokenizer(features):
     #USING padding = "max_length fails with gpt2."
-    return autotokenizer(features['instruction'],  truncation = True)
+    inputs = autotokenizer(features['text'], truncation = True, padding  ="max_length", max_length = 128)
+    inputs['labels'] = inputs['input_ids'].copy()
+    return inputs
 
 def calculate_metrics(eval_pred):
     logits,labels = eval_pred
     predictions = np.argmax(logits,axis = 1)
     return metric.compute(predictions = predictions, references = labels)
 
-print(dataset)
+
+dataset = load_dataset('wikitext', 'wikitext-2-raw-v1')
+
 autotokenizer = AutoTokenizer.from_pretrained("gpt2")
+autotokenizer.pad_token = autotokenizer.eos_token
+
 tokenized = dataset.map(function_tokenizer, batched = True)
-model = GPT2Model.from_pretrained("EleutherAI/gpt-neo-1.3")
+model = GPT2Model.from_pretrained("gpt2")
 
 training_args = TrainingArguments(output_dir = "test_trainer", eval_strategy = "epoch")
-metric.evaluate.load("accuracy")
+metric = evaluate.load("accuracy")
 
 trainer = Trainer(
     model = model,
@@ -38,7 +41,6 @@ trainer = Trainer(
     compute_metrics=calculate_metrics
 
 )
-
 trainer.train()
 
 
