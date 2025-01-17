52f179de015e585f075a69bd09b4f16fe90961bc
Luis Miguel Pérez <perezmartluismiguel@gmail.com>
2025-01-17 23:27:30 +0100

    Add files via upload

--- dataprepare.py
+++ dataprepare.py
@@ -1,18 +1,16 @@
 #SCRIPT TO PREPARE DATASET FOR A CHATBOT AI
 import pandas as pd
-from transformers import AutoTokenizer, TrainingArguments, Trainer
+from transformers import AutoTokenizer, TrainingArguments, Trainer, GPT2ForSequenceClassification
 from transformers import GPT2Model
 from datasets import load_dataset
 import evaluate
-
-
-
-
+import numpy as np
+import sklearn
 
 
 def function_tokenizer(features):
     #USING padding = "max_length fails with gpt2."
-    inputs = autotokenizer(features['text'], truncation = True, padding  ="max_length", max_length = 128)
+    inputs = autotokenizer(features['text'], truncation = True, max_length = 128)
     inputs['labels'] = inputs['input_ids'].copy()
     return inputs
 
@@ -23,12 +21,14 @@ def calculate_metrics(eval_pred):
 
 
 dataset = load_dataset('wikitext', 'wikitext-2-raw-v1')
+train_dataset = dataset['train']
+print(dataset)
 
 autotokenizer = AutoTokenizer.from_pretrained("gpt2")
 autotokenizer.pad_token = autotokenizer.eos_token
 
-tokenized = dataset.map(function_tokenizer, batched = True)
-model = GPT2Model.from_pretrained("gpt2")
+tokenized = train_dataset.map(function_tokenizer)
+model = GPT2ForSequenceClassification.from_pretrained("gpt2")
 
 training_args = TrainingArguments(output_dir = "test_trainer", eval_strategy = "epoch")
 metric = evaluate.load("accuracy")
@@ -42,5 +42,6 @@ trainer = Trainer(
 
 )
 trainer.train()
+
 
 
